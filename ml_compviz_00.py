22856edbdc59bfc954e60b44300449638b5a044f
KDAB1175 <88834793+KDAB1175@users.noreply.github.com>
2023-04-17 21:45:05 +0200

    Update ml_compviz_00.py

--- ml_compviz_00.py
+++ ml_compviz_00.py
@@ -7,7 +7,66 @@ mnist = tf.keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
-plt.imshow(training_images[0])
+#normal scenario
+plt.imshow(training_images[42])
 
-print(training_labels[0])
-print(training_images[0])
+print(training_labels[42])
+print(training_images[42])
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=50)
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+print(classifications[0])
+
+print("break---------------------------------------------------------------------------------------------------")
+
+#values left outside of the 0.0 and 1.0 range
+training_images  = training_images * 255.0
+test_images = test_images * 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
+
+print("break---------------------------------------------------------------------------------------------------")
+
+#stopping training when accuracy reaches 95%
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.95):
+      print("\nReached 95% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
